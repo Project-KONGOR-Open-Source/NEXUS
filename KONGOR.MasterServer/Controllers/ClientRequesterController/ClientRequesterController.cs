d13e3dc8dc9cb8d86003f474906a5205c23299e3
[K]ONGOR <project.kongor@proton.me>
2023-12-25 22:56:58 +0000

    Describe The Client Requester Controller

--- KONGOR.MasterServer/Controllers/ClientRequesterController/ClientRequesterController.cs
+++ KONGOR.MasterServer/Controllers/ClientRequesterController/ClientRequesterController.cs
@@ -5,11 +5,20 @@
 [Consumes("application/x-www-form-urlencoded")]
 public partial class ClientRequesterController : ControllerBase
 {
-    /// <summary>
-    ///     Requests hitting the Client Requester endpoint have two types of request identifiers.
-    ///     1) Part of the query string, in the format "client_requester.php?f={function}".
-    ///     2) Specified as the form data request parameter "f", in the format "f={function}".
-    /// </summary>
+    # region Client Requester Controller Description
+    /*
+        Requests hitting the Client Requester endpoint are uniquely identified by one of two ways to specify the PHP function that they are trying to invoke.
+        The PHP function identifier is specified as either 1) the query string parameter "f", in the format "client_requester.php?f={function}", or 2) the form parameter "f", in the format "f={function}".
+
+        Additionally, the Client Requester controller is massive due to the Master Server API not being RESTful.
+        This creates an integration complication, due to the fact that MVC action methods cannot be overloaded to the degree that the Master Server API requires it.
+        The problem is that MVC action methods are uniquely identified by the combination of route and request method.
+        And while the query string and/or the form data of requests hitting this endpoint can be different, the route is always "client_requester.php" and the method is always POST.
+        This means that it is not possible to write individual action methods that uniquely identify the request by query string parameter or form data parameter.
+        And instead the Client Requester controller needs to be handled using switch pattern matching, or equivalent, on these request data.
+    */
+    # endregion
+
     [HttpPost(Name = "Client Requester All-In-One")]
     public async Task<IActionResult> ClientRequester()
     {
