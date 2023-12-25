efac0a6fb227e270b897941f29a1297903dc82a2
[K]ONGOR <project.kongor@proton.me>
2023-12-25 20:43:53 +0000

    Clean Up Entry Point Files

--- KONGOR.MasterServer/KONGOR.cs
+++ KONGOR.MasterServer/KONGOR.cs
@@ -4,29 +4,31 @@ internal class KONGOR
 {
     internal static void Main(string[] args)
     {
-        //KongorContext.ServerStartEpochTime = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
+        // Sets The Server Start Epoch Time (This Will Stop Working In 2038)
+        // KongorContext.ServerStartEpochTime = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds();
 
-        // Build The Application
+        // Create The Application Builder
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
         // Add Aspire Service Defaults
         builder.AddServiceDefaults();
 
         // Add The Database Context
-        builder.Services.AddDbContext<MerrickContext>(options => { options.UseSqlServer("MERRICK Database"); });
+        builder.Services.AddDbContext<MerrickContext>(options =>
+        {
+            // Set The Database Connection Options
+            options.UseSqlServer("MERRICK Database", connection => connection.MigrationsAssembly("MERRICK.Database.Manager"));
+
+            // Enable Comprehensive Database Query Logging
+            options.EnableSensitiveDataLogging(builder.Environment.IsDevelopment());
+        });
 
         // Add MVC Controllers
         builder.Services.AddControllers();
 
-        //builder.Services.AddEndpointsApiExplorer();
-
-        //builder.Services.AddIdentityCore<User>()
-        //    .AddRoles<Role>()
-        //    .AddEntityFrameworkStores<MerrickContext>()
-        //    .AddDefaultTokenProviders();
-
-        // builder.Services.AddEndpointsApiExplorer();
-        // builder.Services.AddProblemDetails();
+        // Add Comprehensive Error Responses
+        if (builder.Environment.IsDevelopment())
+            builder.Services.AddProblemDetails();
 
         // Add Swagger
         builder.Services.AddSwaggerGen();
@@ -61,67 +63,31 @@ internal class KONGOR
         //            new List<string>()
         //        }
         //    });
+
+        /*
+         *
+         *    options.SwaggerDoc("v1", new OpenApiInfo
+           {
+               Version = "v1",
+               Title = "ToDo API",
+               Description = "An ASP.NET Core Web API for managing ToDo items",
+               TermsOfService = new Uri("https://example.com/terms"),
+               Contact = new OpenApiContact
+               {
+                   Name = "Example Contact",
+                   Url = new Uri("https://example.com/contact")
+               },
+               License = new OpenApiLicense
+               {
+                   Name = "Example License",
+                   Url = new Uri("https://example.com/license")
+               }
+           });
+         *
+         */
         //});
 
-        //builder.Services.AddExceptionHandler();
-
-        //if (builder.Environment.IsDevelopment())
-        //{
-        //    builder.Services.Configure<IdentityOptions>(options =>
-        //    {
-        //        options.User.RequireUniqueEmail = false;
-        //        options.User.AllowedUserNameCharacters = string.Concat(Enumerable.Range(char.MinValue, char.MaxValue).Select(Convert.ToChar).Where(character => char.IsControl(character).Equals(false)));
-
-        //        options.Password.RequiredLength = 0;
-        //        options.Password.RequiredUniqueChars = 0;
-        //        options.Password.RequireNonAlphanumeric = false;
-        //        options.Password.RequireLowercase = false;
-        //        options.Password.RequireUppercase = false;
-        //        options.Password.RequireDigit = false;
-
-        //        options.Lockout.MaxFailedAccessAttempts = 5;
-        //        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.Zero;
-        //    });
-        //}
-
-        //else
-        //{
-        //    builder.Services.Configure<IdentityOptions>(options =>
-        //    {
-        //        options.User.RequireUniqueEmail = true;
-        //        options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
-
-        //        options.Password.RequiredLength = 8;
-        //        options.Password.RequiredUniqueChars = 4;
-        //        options.Password.RequireNonAlphanumeric = true;
-        //        options.Password.RequireLowercase = true;
-        //        options.Password.RequireUppercase = true;
-        //        options.Password.RequireDigit = true;
-
-        //        options.Lockout.MaxFailedAccessAttempts = 3;
-        //        options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15);
-        //    });
-        //}
-
-        //builder.Services.AddAuthentication(options =>
-        //{
-        //    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        //    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        //}).AddJwtBearer(options =>
-        //{
-        //    options.TokenValidationParameters = new TokenValidationParameters
-        //    {
-        //        ValidateIssuerSigningKey = true,
-        //        ValidateIssuer = true,
-        //        ValidateAudience = true,
-        //        ValidateLifetime = true,
-        //        ClockSkew = TimeSpan.Zero,
-        //        ValidIssuer = builder.Configuration["JWT:Issuer"],
-        //        ValidAudience = builder.Configuration["JWT:Audience"],
-        //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])) // TODO: Put The Key In A Secrets File And Move That File To A Separate Repository
-        //    };
-        //});
-
+        // Build The Application
         WebApplication app = builder.Build();
 
         if (app.Environment.IsDevelopment())
@@ -135,14 +101,11 @@ internal class KONGOR
                 // https://amoenus.dev/swagger-dark-theme
             });
         }
+
         else
         {
             app.UseExceptionHandler();
         }
-
-        //app.UseHttpsRedirection();
-
-        //app.UseAuthorization();
 
         // Map Aspire Default Endpoints
         app.MapDefaultEndpoints();
--- MERRICK.Database.Manager/MERRICK.cs
+++ MERRICK.Database.Manager/MERRICK.cs
@@ -4,26 +4,39 @@ internal class MERRICK
 {
     internal static async Task Main(string[] args)
     {
+        // Create The Application Builder
         WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+        // Add Aspire Service Defaults
         builder.AddServiceDefaults();
 
+        // Get The Database Connection String
         string connectionString = builder.Configuration.GetConnectionString("MERRICK") ?? throw new NullReferenceException("MERRICK Connection String Is NULL");
 
+        // Add The Database Context
         builder.Services.AddDbContext<MerrickContext>(options =>
         {
+            // Set The Database Connection Options
             options.UseSqlServer(connectionString, connection => connection.MigrationsAssembly(typeof(MERRICK).Assembly.GetName().Name));
+
+            // Enable Comprehensive Database Query Logging
+            options.EnableSensitiveDataLogging(builder.Environment.IsDevelopment());
         });
+
+        // TODO: Document The Services Below
 
         builder.Services.AddOpenTelemetry().WithTracing(tracing => tracing.AddSource(DatabaseInitializer.ActivitySourceName));
         builder.Services.AddSingleton<DatabaseInitializer>();
         builder.Services.AddHostedService(provider => provider.GetRequiredService<DatabaseInitializer>());
         builder.Services.AddHealthChecks().AddCheck<DatabaseHealthCheck>("DatabaseHealthCheck", null);
 
+        // Build The Application
         WebApplication application = builder.Build();
 
+        // Map Aspire Default Endpoints
         application.MapDefaultEndpoints();
 
+        // Run The Application
         await application.RunAsync();
     }
 }
