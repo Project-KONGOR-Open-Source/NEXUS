1e3675cbb3d4721fd201e9fd3763b7289546c78f
[K]ONGOR <project.kongor@proton.me>
2023-12-25 20:44:18 +0000

    Add Catch-All Client Requester Controller

---
+++ KONGOR.MasterServer/Controllers/ClientRequesterController/Base/BaseClientRequesterController.cs
@@ -0,0 +1,21 @@
+﻿namespace KONGOR.MasterServer.Controllers.ClientRequesterController.Base;
+
+[ApiController]
+[Route("client_requester.php")]
+[Consumes("application/x-www-form-urlencoded")]
+public class BaseClientRequesterController : ControllerBase
+{
+    /// <summary>
+    ///     Requests hitting the Client Requester endpoint have two types of request identifiers.
+    ///     1) Part of the query string, in the format "client_requester.php?f={endpoint}".
+    ///     2) Specified as the form data request parameter "f", in the format "f={endpoint}".
+    /// </summary>
+    [HttpPost(Name = "Client Requester")]
+    public async Task<IActionResult> ClientRequester([FromForm] Dictionary<string, string> formData)
+    {
+        string? functionName;
+        string[] f = [.. Request.Query["f"]];
+
+        return Ok();
+    }
+}
