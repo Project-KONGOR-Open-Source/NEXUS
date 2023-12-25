24224cc9f03acad29a7b9330d71ec76c3b40c8da
[K]ONGOR <project.kongor@proton.me>
2023-12-25 22:25:56 +0000

    Create Pattern For Building The (Massive) Client Requester Controller

---
+++ KONGOR.MasterServer/Controllers/ClientRequesterController/ClientRequesterController.cs
@@ -0,0 +1,32 @@
+﻿namespace KONGOR.MasterServer.Controllers.ClientRequesterController;
+
+[ApiController]
+[Route("client_requester.php")]
+[Consumes("application/x-www-form-urlencoded")]
+public partial class ClientRequesterController : ControllerBase
+{
+    /// <summary>
+    ///     Requests hitting the Client Requester endpoint have two types of request identifiers.
+    ///     1) Part of the query string, in the format "client_requester.php?f={function}".
+    ///     2) Specified as the form data request parameter "f", in the format "f={function}".
+    /// </summary>
+    [HttpPost(Name = "Client Requester All-In-One")]
+    public async Task<IActionResult> ClientRequester()
+    {
+        return Request.Query["f"].SingleOrDefault() switch
+        {
+            "pre_auth"  => await HandlePreAuthenticationFunction(),
+            "srpAuth"   => await HandleSRPAuthenticationFunction(),
+            null        => await HandleNullQueryStringFunctionParameter(),
+            _           => throw new NotImplementedException($"Unsupported Client Requester Controller Query String Parameter: f={Request.Query["f"].Single()}")
+        };
+    }
+
+    private async Task<IActionResult> HandleNullQueryStringFunctionParameter()
+    {
+        return Request.Form["f"].SingleOrDefault() switch
+        {
+            _           => throw new NotImplementedException($"Unsupported Client Requester Controller Form Parameter: f={Request.Query["f"].Single()}")
+        };
+    }
+}
---
+++ KONGOR.MasterServer/Controllers/ClientRequesterController/ClientRequesterControllerAuthentication.cs
@@ -0,0 +1,14 @@
+﻿namespace KONGOR.MasterServer.Controllers.ClientRequesterController;
+
+public partial class ClientRequesterController
+{
+    private async Task<IActionResult> HandlePreAuthenticationFunction()
+    {
+        return Ok();
+    }
+
+    private async Task<IActionResult> HandleSRPAuthenticationFunction()
+    {
+        return Ok();
+    }
+}
