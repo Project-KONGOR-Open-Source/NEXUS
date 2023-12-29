4c393ff1c46998593f3199f78ec4123810882fd6
[K]ONGOR <project.kongor@proton.me>
2023-12-29 16:30:44 +0000

    Add Placeholder Web Portal API

--- ASPIRE.AppHost/ASPIRE.cs
+++ ASPIRE.AppHost/ASPIRE.cs
@@ -10,9 +10,8 @@ internal class KONGOR
 
         IResourceBuilder<RedisContainerResource> cache = builder.AddRedisContainer("REDIS Cache");
 
-        builder.AddProject<KONGOR_MasterServer>("KONGOR Master Server")
-            .WithReference(database)
-            .WithReference(cache);
+        builder.AddProject<KONGOR_MasterServer>("KONGOR Master Server").WithReference(database);
+        builder.AddProject<ZORGATH_WebPortal_API>("ZORGATH Web Portal API").WithReference(database);
 
         builder.Build().Run();
     }
