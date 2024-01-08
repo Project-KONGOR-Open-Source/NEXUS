1a43d6aa6a78eecce984ec64439729b00daa8cf7
[K]ONGOR <project.kongor@proton.me>
2024-01-08 13:17:43 +0000

    Minor Update To Assembly Attributes Description

--- ASPIRE.Tests/Internals/AssemblyAttributes.cs
+++ ASPIRE.Tests/Internals/AssemblyAttributes.cs
@@ -1,7 +1,8 @@
 ﻿// Parallelisation In NUnit Is Disabled By Default
+// Uncomment This Attribute To Enable Parallelisation
 [assembly: Parallelizable(ParallelScope.Fixtures)]
 
 // This Attribute Is Optional; If It Is Not Specified, NUnit Uses The Processor Count Or 2, Whichever Is Greater
 // For Example, On A Four Processor Machine, The Default Value Is 4
-// Enable This Attribute To Disable Parallelisation But Keep The Execution Scope Defined By The "Parallelizable" Attribute
+// Uncomment This Attribute To Serialise Test Execution, But Keep The Execution Scope Defined By The "Parallelizable" Attribute
 // [assembly: LevelOfParallelism(1)]
