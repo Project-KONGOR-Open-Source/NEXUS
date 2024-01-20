be0691e47c330200e23cb829796dc1dec0098120
[K]ONGOR <project.kongor@proton.me>
2024-01-20 00:04:42 +0000

    Expand On Account Type Definitions

--- MERRICK.Database/Enumerations/AccountType.cs
+++ MERRICK.Database/Enumerations/AccountType.cs
@@ -2,12 +2,18 @@
 
 public enum AccountType
 {
-    Disabled = 0,   // prevented from logging into HON
-    Normal = 3,     // free-to-play account
-    Legacy = 4,     // purchased HON during the beta
-    Staff = 5,      // has access to admin functions and can execute admin commands
+    // built-in types
+    Disabled            = 0,    // prevented from logging into HoN
+    Trial               = 1,    // has limited access to in-game functions
+    ServerHost          = 2,    // has permissions to host game servers
+    Normal              = 3,    // free-to-play account
+    Legacy              = 4,    // purchased HoN during the beta
+    Staff               = 5,    // has access to admin functions and can execute admin commands
+    GameMaster          = 6,    // can suspend players
+    MatchModerator      = 7,    // can spectate and moderate matches
+    MatchCaster         = 8,    // can spectate matches
 
-    Streamer = 101, // has top priority in the matchmaking queue
-    VIP = 102,      // has top priority in the matchmaking queue
-    Caster = 100    // has the "Staff Spectate" permission
+    // custom types
+    Streamer            = 101,  // has top priority in the matchmaking queue
+    VIP                 = 102   // has top priority in the matchmaking queue
 }
