27f487918d984bd9655aef5d1f20a5d58830784a
[K]ONGOR <project.kongor@proton.me>
2024-02-06 08:55:42 +0000

    Add Placeholder Controllers

---
+++ KONGOR.MasterServer/Controllers/PatcherController/PatcherController.cs
@@ -0,0 +1,15 @@
+﻿namespace KONGOR.MasterServer.Controllers.PatcherController;
+
+[ApiController]
+[Route("patcher/patcher.php")]
+[Consumes("application/x-www-form-urlencoded")]
+public class PatcherController : ControllerBase
+{
+    [HttpPost(Name = "Patcher")]
+    public IActionResult Patcher()
+    {
+        // TODO: Implement Patcher Controller
+
+        return Ok(@"a:5:{i:0;a:8:{s:7:""version"";s:6:""4.10.1"";s:2:""os"";s:3:""wac"";s:4:""arch"";s:6:""x86_64"";s:3:""url"";s:42:""http://cdn.naeu.patch.heroesofnewerth.com/"";s:4:""url2"";s:42:""http://cdn.naeu.patch.heroesofnewerth.com/"";s:14:""latest_version"";s:6:""4.10.1"";s:24:""latest_manifest_checksum"";s:40:""33b5151fca1704aff892cf76e41f3986634d38bb"";s:20:""latest_manifest_size"";s:7:""3628533"";}s:7:""version"";s:8:""4.10.1.0"";s:15:""current_version"";s:8:""4.10.1.0"";s:25:""current_manifest_checksum"";s:40:""33b5151fca1704aff892cf76e41f3986634d38bb"";s:21:""current_manifest_size"";s:7:""3628533"";}");
+    }
+}
---
+++ KONGOR.MasterServer/Controllers/StorageStatusController/StorageStatusController.cs
@@ -0,0 +1,15 @@
+﻿namespace KONGOR.MasterServer.Controllers.StorageStatusController;
+
+[ApiController]
+[Route("master/storage/status")]
+[Consumes("application/x-www-form-urlencoded")]
+public class StorageStatusController : ControllerBase
+{
+    [HttpPost(Name = "Storage Status")]
+    public IActionResult StorageStatus([FromForm] Dictionary<string, string> formData)
+    {
+        // TODO: Implement Storage Status Controller
+
+        return Ok(@"a:4:{s:7:""success"";b:1;s:4:""data"";N;s:18:""cloud_storage_info"";a:4:{s:10:""account_id"";s:6:""195592"";s:9:""use_cloud"";s:1:""0"";s:16:""cloud_autoupload"";s:1:""0"";s:16:""file_modify_time"";s:19:""2021-01-10 11:39:47"";}s:8:""messages"";s:0:"""";}");
+    }
+}
