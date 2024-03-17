5aa71ea2147cf69b9859ee98343ff27a0a503033
[K]ONGOR <project.kongor@proton.me>
2024-03-17 19:22:45 +0000

    Make Service Names Aspire-Compliant

--- ASPIRE.AppHost/ASPIRE.cs
+++ ASPIRE.AppHost/ASPIRE.cs
@@ -6,20 +6,20 @@ public class ASPIRE
     {
         IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-        builder.AddProject<MERRICK_Database>("MERRICK Database")
+        builder.AddProject<MERRICK_Database>("database")
             .WithLaunchProfile(builder.Environment.IsProduction() ? "MERRICK.Database Production" : "MERRICK.Database Development");
 
         // TODO: Add Chat Server Launch Profile
 
-        // builder.AddProject<TRANSMUTANSTEIN_ChatServer>("TRANSMUTANSTEIN Chat Server")
+        // builder.AddProject<TRANSMUTANSTEIN_ChatServer>("chat-server")
         //     .WithLaunchProfile(builder.Environment.IsProduction() ? "TRANSMUTANSTEIN.ChatServer Production" : "TRANSMUTANSTEIN.ChatServer Development");
 
         // TODO: Try To Set The Connection String Via Environment Variable
 
-        builder.AddProject<KONGOR_MasterServer>("KONGOR Master Server")
+        builder.AddProject<KONGOR_MasterServer>("master-server")
             .WithLaunchProfile(builder.Environment.IsProduction() ? "KONGOR.MasterServer Production" : "KONGOR.MasterServer Development");
 
-        builder.AddProject<ZORGATH_WebPortal_API>("ZORGATH Web Portal API")
+        builder.AddProject<ZORGATH_WebPortal_API>("web-portal-api")
             .WithLaunchProfile(builder.Environment.IsProduction() ? "ZORGATH.WebPortal.API Production" : "ZORGATH.WebPortal.API Development");
 
         builder.Build().Run();
