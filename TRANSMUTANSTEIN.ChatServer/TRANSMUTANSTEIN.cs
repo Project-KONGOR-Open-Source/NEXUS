ce3bb338a4a7c81397e6de8cd53428b5e1237480
[K]ONGOR <project.kongor@proton.me>
2024-03-31 18:43:16 +0100

    Simplify Server Configuration

--- TRANSMUTANSTEIN.ChatServer/TRANSMUTANSTEIN.cs
+++ TRANSMUTANSTEIN.ChatServer/TRANSMUTANSTEIN.cs
@@ -3,46 +3,27 @@
 using Microsoft.AspNetCore.SignalR;
 
 using TRANSMUTANSTEIN.ChatServer.Hubs;
-using TRANSMUTANSTEIN.ChatServer.Services;
 
 namespace TRANSMUTANSTEIN.ChatServer;
 
 public class TRANSMUTANSTEIN
 {
-    public static Task Main(string[] args)
+    public static void Main(string[] args)
     {
-        var config = new ConfigurationBuilder()
-            .AddCommandLine(args)
-            .Build();
+        var builder = WebApplication.CreateBuilder(args);
 
-        var host = Host.CreateDefaultBuilder(args)
-            .ConfigureWebHost(webHostBuilder =>
-            {
-                webHostBuilder
-                    .UseConfiguration(config)
-                    .UseSetting(WebHostDefaults.PreventHostingStartupKey, "true")
-                    .ConfigureLogging((c, factory) =>
-                    {
-                        factory.AddConfiguration(c.Configuration.GetSection("Logging"));
-                        factory.AddConsole();
-                        factory.SetMinimumLevel(LogLevel.Debug);
-                    })
-                    .UseKestrel(options =>
-                    {
-                        // Default port
-                        // options.ListenAnyIP(11031);
+        builder.Services.AddSignalR();
 
-                        // Hub bound to TCP end point
-                        options.Listen(IPAddress.Any, 11031, builder =>
-                        {
-                            builder.UseHub<Chat>();
-                        });
-                    })
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseIISIntegration()
-                    .UseStartup<Startup>();
-            }).Build();
+        builder.WebHost.UseKestrel(options =>
+        {
+            options.Listen(IPAddress.Any, 55507 /* TODO: Get From Configuration */, configure => { configure.UseHub<Chat>(); });
+            options.Listen(IPAddress.Any, 55508 /* TODO: Get From Configuration */, configure => { configure.UseHub<Chat>(); configure.UseHttps(); });
+        });
 
-        return host.RunAsync();
+        var app = builder.Build();
+
+        app.MapHub<ChatHub>(string.Empty);
+
+        app.Run();
     }
 }
