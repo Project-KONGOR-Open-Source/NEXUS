2344afaf30c1382c8bbe8e40ea4caf48afcd0181
[K]ONGOR <project.kongor@proton.me>
2024-04-01 15:41:57 +0100

    Establish Chat Server Connection

--- TRANSMUTANSTEIN.ChatServer/Core/ChatBuffer.cs
+++ TRANSMUTANSTEIN.ChatServer/Core/ChatBuffer.cs
@@ -76,4 +76,55 @@ public class ChatBuffer // TODO: Inherit From TCPBuffer And Consolidate The Two 
         Buffer[finalWriteOffset - 1] = 0;
         Size = finalWriteOffset;
     }
+
+    /*
+       public static byte ReadByte(byte[] data, int offset, out int updatedOffset)
+       {
+           byte result = data[offset];
+           updatedOffset = offset + 1;
+           return result;
+       }
+
+       public static short ReadShort(byte[] data, int offset, out int updatedOffset)
+       {
+           short result = BitConverter.ToInt16(data, offset);
+           updatedOffset = offset + 2;
+           return result;
+       }
+
+       public static int ReadInt(byte[] data, int offset, out int updatedOffset)
+       {
+           int result = BitConverter.ToInt32(data, offset);
+           updatedOffset = offset + 4;
+           return result;
+       }
+       public static long ReadLong(byte[] data, int offset, out int updatedOffset)
+       {
+           long result = BitConverter.ToInt64(data, offset);
+           updatedOffset = offset + 8;
+           return result;
+       }
+       public static float ReadFloat(byte[] data, int offset, out int updatedOffset)
+       {
+           return BitConverter.Int32BitsToSingle(ReadInt(data, offset, out updatedOffset));
+       }
+
+       public static string ReadString(byte[] data, int offset, out int updatedOffset)
+       {
+           int end = offset;
+           while (true)
+           {
+               byte b = data[end];
+               if (b == 0)
+               {
+                   break;
+               }
+               ++end;
+           }
+           string result = Encoding.UTF8.GetString(data, offset, end - offset);
+
+           updatedOffset = end + 1; // skip \0
+           return result;
+       }
+     */
 }
--- TRANSMUTANSTEIN.ChatServer/Core/ChatSession.cs
+++ TRANSMUTANSTEIN.ChatServer/Core/ChatSession.cs
@@ -15,22 +15,32 @@ public class ChatSession(TCPServer server) : TCPSession(server)
     protected override void OnReceived(byte[] buffer, long offset, long size)
     {
         string message = Encoding.UTF8.GetString(buffer, (int)offset, (int)size);
-        Console.WriteLine($"Incoming: {message}");
 
-        // Temporarily Force Accepting The Connection, Until Implemented Properly
-        // "\0" is the NULL character, which has the value 0 in the ASCII table and is used to determine the end of C-style strings (also known as the NULL Terminator)
-        if (message.EndsWith("\0en\0en\0"))
+        Console.WriteLine(size is 2 ? $"Incoming Size: {buffer[0]}, {buffer[1]}" : $"Incoming: {message}");
+
+        const short connectionRequest = 0x0C00;
+        byte[] connectionRequestBytes = BitConverter.GetBytes(connectionRequest);
+
+        if (buffer.AsSpan()[..2].SequenceEqual(connectionRequestBytes.AsSpan()))
         {
-            //ChatBuffer response = new();
-            //const short accept = 0x1C00;
-            //response.WriteInt16(accept);
-            //SendAsync(response.Buffer);
+            const short connectionAccept = 0x1C00;
+            byte[] connectionAcceptBytes = BitConverter.GetBytes(connectionAccept);
 
-            TCPBuffer response = new();
-            const short accept = 0x1C00;
-            response.Append(BitConverter.GetBytes(accept));
-            SendAsync(response.Data);
+            // All Packets End In A "0"
+
+            byte[] responseSize = [ 3, 0 ];
+            byte[] responseData = [ connectionAcceptBytes[0], connectionAcceptBytes[1], 0 ];
+
+            // Send The Size Of The Next Packet
+            SendAsync(responseSize);
+            Console.WriteLine($"Outgoing Size: {responseSize[0]}, {responseSize[1]}");
+
+            // Send The "Accept Connection" Packet
+            SendAsync(responseData);
+            Console.WriteLine(Encoding.UTF8.GetString(responseData, 0, responseData.Length));
         }
+
+        // For strings, "\0" is the NULL character, which has the value 0 in the ASCII table and is used to determine the end of C-style strings (also known as the NULL Terminator).
     }
 
     protected override void OnError(SocketError error)
