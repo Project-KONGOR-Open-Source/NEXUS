ef4eab83052fef836a52f06106de75dcefa7a95e
[K]ONGOR <project.kongor@proton.me>
2024-04-28 19:55:12 +0100

    Remove Unused Chat Server Protocol Configuration Option

--- ASPIRE.AppHost/ASPIRE.cs
+++ ASPIRE.AppHost/ASPIRE.cs
@@ -8,9 +8,6 @@ public class ASPIRE
 
         # region Chat Server Configuration
 
-        string chatServerProtocol = builder.Configuration.GetRequiredSection("ChatServer").GetValue<string?>("Protocol")
-            ?? throw new NullReferenceException("Chat Server Protocol Is NULL");
-
         string chatServerHost = builder.Configuration.GetRequiredSection("ChatServer").GetValue<string?>("Host")
             ?? throw new NullReferenceException("Chat Server Host Is NULL");
 
@@ -38,12 +35,12 @@ public class ASPIRE
         builder.AddProject<KONGOR_MasterServer>("master-server", builder.Environment.IsProduction() ? "KONGOR.MasterServer Production" : "KONGOR.MasterServer Development")
             .WithReference(databaseConnectionString)
             .WithReference(distributedCache, connectionName: "GARNET")
-            .WithEnvironment("CHAT_SERVER_PROTOCOL", chatServerProtocol).WithEnvironment("CHAT_SERVER_ADDRESS", chatServerAddress).WithEnvironment("CHAT_SERVER_PORT", chatServerPort.ToString());
+            .WithEnvironment("CHAT_SERVER_ADDRESS", chatServerAddress).WithEnvironment("CHAT_SERVER_PORT", chatServerPort.ToString());
 
         builder.AddProject<TRANSMUTANSTEIN_ChatServer>("chat-server", builder.Environment.IsProduction() ? "TRANSMUTANSTEIN.ChatServer Production" : "TRANSMUTANSTEIN.ChatServer Development")
             .WithReference(databaseConnectionString)
             .WithReference(distributedCache, connectionName: "GARNET")
-            .WithEnvironment("CHAT_SERVER_PROTOCOL", chatServerProtocol).WithEnvironment("CHAT_SERVER_ADDRESS", chatServerAddress).WithEnvironment("CHAT_SERVER_PORT", chatServerPort.ToString());
+            .WithEnvironment("CHAT_SERVER_ADDRESS", chatServerAddress).WithEnvironment("CHAT_SERVER_PORT", chatServerPort.ToString());
 
         builder.AddProject<ZORGATH_WebPortal_API>("web-portal-api", builder.Environment.IsProduction() ? "ZORGATH.WebPortal.API Production" : "ZORGATH.WebPortal.API Development")
             .WithReference(databaseConnectionString);
