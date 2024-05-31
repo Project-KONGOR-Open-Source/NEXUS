6767ee9e261c7213986eb7acdb1c3c3d8a09b75e
[K]ONGOR <project.kongor@proton.me>
2024-06-01 00:28:45 +0100

    Cast Nullable Match Server Type To Non-Nullable

--- KONGOR.MasterServer/Controllers/ClientRequesterController/ClientRequesterControllerServerList.cs
+++ KONGOR.MasterServer/Controllers/ClientRequesterController/ClientRequesterControllerServerList.cs
@@ -12,7 +12,7 @@ public partial class ClientRequesterController
         string? gameType = Request.Form.ContainsKey("gametype") ? Request.Form["gametype"].ToString() : null;
 
         List<MatchServer> servers = (await DistributedCache.HashGetAllAsync("MATCH-SERVERS"))
-            .Select(entry => JsonSerializer.Deserialize<MatchServer>(entry.Value.ToString()) ?? throw new NullReferenceException("Deserialized Match Server Is NULL")).ToList();
+            .Select(entry => JsonSerializer.Deserialize<MatchServer>(entry.Value.ToString())).OfType<MatchServer>().ToList();
 
         switch (gameType)
         {
