c3e644c87d61a1fec485cd38a7420e27ce171ec8
[K]ONGOR <project.kongor@proton.me>
2024-05-31 23:17:48 +0100

    Temporarily Disable Server Status Filter

--- KONGOR.MasterServer/Models/RequestResponse/ServerManagement/ServerListResponse.cs
+++ KONGOR.MasterServer/Models/RequestResponse/ServerManagement/ServerListResponse.cs
@@ -56,7 +56,7 @@ public class ServerForJoinListResponse(List<MatchServer> servers, string cookie)
 {
     [PhpProperty("server_list")]
     public Dictionary<int, ServerForJoin> Servers { get; set; } = servers.Any() is false ? []
-        : servers.Where(server => server.Status is ServerStatus.SERVER_STATUS_ACTIVE)
+        : servers // .Where(server => server.Status is ServerStatus.SERVER_STATUS_ACTIVE) // TODO: Filter By Server Status ... Maybe!?
             .ToDictionary(server => server.ID, server => new ServerForJoin(server.ID.ToString(), server.IPAddress, server.Port.ToString(), server.Location));
 }
 
