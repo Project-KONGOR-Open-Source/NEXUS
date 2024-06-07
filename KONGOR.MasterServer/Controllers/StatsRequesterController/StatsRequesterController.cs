4d96e3668eb77f3675323b339b01db81dcb3c081
[K]ONGOR <project.kongor@proton.me>
2024-06-07 23:21:08 +0100

    Add Initial Stats Requester Controller Form Data Handlers

--- KONGOR.MasterServer/Controllers/StatsRequesterController/StatsRequesterController.cs
+++ KONGOR.MasterServer/Controllers/StatsRequesterController/StatsRequesterController.cs
@@ -12,6 +12,12 @@ public partial class StatsRequesterController(MerrickContext databaseContext, IC
 
     private string MatchStatsSubmissionSalt => "s8c7xaduxAbRanaspUf3kadRachecrac9efeyupr8suwrewecrUphayeweqUmana";
 
+    // For Debugging Purposes, The "GiveGold" And "GiveExp" Commands (Case-Insensitive) Can Be Used From The Server Console To Complete Matches Quickly And Send Stats
+    // e.g. #1: "givegold 0 65535" To Give 65535 Gold To Player Index 0 (The First Player), Or "givegold KONGOR 65535" To Give 65535 Gold To Player With Name "KONGOR"
+    // e.g. #2: "giveexp KONGOR 65535" To Give 65535 Experience To Player With Name "KONGOR" (Unlike The "GiveGold" Command, The "GiveExp" Command Does Not Work With A Player Index)
+    // NOTE #1: 1v1 Matches Are A Good Way To Test The Stats Submission System, As They Are The Quickest To Complete
+    // NOTE #2: Another Quick Way To Test The Stats Submission System Is To Replay A Fiddler/Requestly/etc. Request Or Make A Postman/Insomnia/etc. Request With The Required Form Data
+
     [HttpPost(Name = "Stats Requester All-In-One")]
     public async Task<IActionResult> StatsRequester()
     {
@@ -23,6 +29,36 @@ public partial class StatsRequesterController(MerrickContext databaseContext, IC
 
         //    return Unauthorized($@"Unrecognized Cookie ""{form.Cookie}""");
         //}
+
+        string? serializedMatchStats = Request.Form["match_stats"];
+
+        if (serializedMatchStats is null)
+            return BadRequest(@"Missing Value For Form Parameter ""match_stats""");
+
+        // TODO: Do Something With Match Stats
+
+        string? serializedTeamStats = Request.Form["team_stats"];
+
+        if (serializedTeamStats is null)
+            return BadRequest(@"Missing Value For Form Parameter ""team_stats""");
+
+        // TODO: Do Something With Team Stats
+
+        string? serializedPlayerStats = Request.Form["player_stats"];
+
+        if (serializedPlayerStats is null)
+            return BadRequest(@"Missing Value For Form Parameter ""player_stats""");
+
+        // TODO: Do Something With Player Stats
+
+        string? serializedInventory = Request.Form["inventory"];
+
+        if (serializedInventory is null)
+            return BadRequest(@"Missing Value For Form Parameter ""inventory""");
+
+        Dictionary<int, Dictionary<string, string>>? inventory = JsonSerializer.Deserialize<Dictionary<int, Dictionary<string, string>>>(serializedInventory);
+
+        // TODO: Do Something With Inventory
 
         return Request.Query["f"].SingleOrDefault() switch
         {
--- KONGOR.MasterServer/Controllers/StatsRequesterController/StatsRequesterControllerSubmit.cs
+++ KONGOR.MasterServer/Controllers/StatsRequesterController/StatsRequesterControllerSubmit.cs
@@ -4,11 +4,46 @@ public partial class StatsRequesterController
 {
     private async Task<IActionResult> HandleStatsSubmission()
     {
+        string? session = Request.Form["session"];
 
+        if (session is null)
+            return BadRequest(@"Missing Value For Form Parameter ""session""");
+
+        // TODO: Validate Session
+
+        return Ok();
     }
 
     private async Task<IActionResult> HandleStatsResubmission()
     {
+        string? login = Request.Form["login"];
 
+        if (login is null)
+            return BadRequest(@"Missing Value For Form Parameter ""login""");
+
+        // TODO: Validate Host Account Name
+
+        string? password = Request.Form["pass"];
+
+        if (password is null)
+            return BadRequest(@"Missing Value For Form Parameter ""pass""");
+
+        // TODO: Validate Host Account Password
+
+        string? resubmissionKey = Request.Form["resubmission_key"];
+
+        if (resubmissionKey is null)
+            return BadRequest(@"Missing Value For Form Parameter ""resubmission_key""");
+
+        // TODO: Do Something With Stats Resubmission Key
+
+        string? serverID = Request.Form["server_id"];
+
+        if (serverID is null)
+            return BadRequest(@"Missing Value For Form Parameter ""server_id""");
+
+        // TODO: Do Something With Server ID
+
+        return Ok();
     }
 }
