bdfafaaa78e552d7ce752f80a9dca40d360e5fe4
[K]ONGOR <project.kongor@proton.me>
2024-06-14 23:29:41 +0100

    Partially Bind Match Stats To Data Model

--- KONGOR.MasterServer/Models/RequestResponse/Stats/StatsForSubmissionRequestForm.cs
+++ KONGOR.MasterServer/Models/RequestResponse/Stats/StatsForSubmissionRequestForm.cs
@@ -2,156 +2,114 @@
 
 public class StatsForSubmissionRequestForm
 {
-    public MatchResults match_stats { get; set; } = new();
+    [FromForm(Name = "match_stats")]
+    public required MatchStats MatchStats { get; set; }
 
-    public Dictionary<int, Dictionary<string, int>> team_stats { get; set; } = new();
+    [FromForm(Name = "team_stats")]
+    public required Dictionary<int, Dictionary<string, int>> TeamStats { get; set; }
 
-    public Dictionary<int, Dictionary<string, PlayerMatchResults>> player_stats { get; set; } = new();
+    [FromForm(Name = "player_stats")]
+    public required Dictionary<int, Dictionary<string, IndividualPlayerStats>> PlayerStats { get; set; }
 
-    public Dictionary<int, Dictionary<int, string>> inventory { get; set; } = new();
+    [FromForm(Name = "inventory")]
+    public required Dictionary<int, Dictionary<int, string>> PlayerInventory { get; set; }
 }
 
-// TODO: Make These Classes Entities
-
-// [Index(nameof(match_id), IsUnique = true)]
-public class MatchResults
+public class MatchStats
 {
-    public int match_id { get; set; }
-    public long server_id { get; set; }
-    public string? map { get; set; }
-    public string? map_version { get; set; }
-    public int time_played { get; set; }
-    public int file_size { get; set; }
-    public string? file_name { get; set; }
-    public int c_state { get; set; }
-    public string? version { get; set; }
-    public int avgpsr { get; set; }
-    public int avgpsr_team1 { get; set; }
-    public int avgpsr_team2 { get; set; }
-    public string? gamemode { get; set; }
-    public int teamscoregoal { get; set; }
-    public int playerscoregoal { get; set; }
-    public int numrounds { get; set; }
-    public string? release_stage { get; set; }
-    public string? banned_heroes { get; set; }
-    public int awd_mann { get; set; }
-    public int awd_mqk { get; set; }
-    public int awd_lgks { get; set; }
-    public int awd_msd { get; set; }
-    public int awd_mkill { get; set; }
-    public int awd_masst { get; set; }
-    public int awd_ledth { get; set; }
-    public int awd_mbdmg { get; set; }
-    public int awd_mwk { get; set; }
-    public int awd_mhdd { get; set; }
-    public int awd_hcs { get; set; }
-    public int mvp { get; set; }
-    public string? submission_debug { get; set; }
+    [FromForm(Name = "server_id")]
+    public required long ServerID { get; set; }
 
-    // Additional field that are not set by the game server and populated manually.
-    public string? name { get; set; } // server name
-    public string? date { get; set; } // DEPRECATED -> TODO: convert this to a [NotMapped] string generated from "datetime"
-    public DateTime datetime { get; set; }
-    public string? time { get; set; } // time in seconds since the day started.
-    public long timestamp { get; set; }  // unix timestamp in seconds since epoch
-    public string? inventory { get; set; }
+    [FromForm(Name = "match_id")]
+    public required int MatchID { get; set; }
+
+    [FromForm(Name = "map")]
+    public required string Map { get; set; }
+
+    [FromForm(Name = "map_version")]
+    public required string MapVersion { get; set; }
+
+    [FromForm(Name = "time_played")]
+    public required int TimePlayed { get; set; }
+
+    [FromForm(Name = "file_size")]
+    public required int FileSize { get; set; }
+
+    [FromForm(Name = "file_name")]
+    public required string FileName { get; set; }
+
+    [FromForm(Name = "c_state")]
+    public required int ConnectionState { get; set; }
+
+    [FromForm(Name = "version")]
+    public required string Version { get; set; }
+
+    [FromForm(Name = "avgpsr")]
+    public required int AveragePSR { get; set; }
+
+    [FromForm(Name = "avgpsr_team1")]
+    public required int AveragePSRTeamOne { get; set; }
+
+    [FromForm(Name = "avgpsr_team2")]
+    public required int AveragePSRTeamTwo { get; set; }
+
+    [FromForm(Name = "gamemode")]
+    public required string GameMode { get; set; }
+
+    [FromForm(Name = "teamscoregoal")]
+    public required int TeamScoreGoal { get; set; }
+
+    [FromForm(Name = "playerscoregoal")]
+    public required int PlayerScoreGoal { get; set; }
+
+    [FromForm(Name = "numrounds")]
+    public required int NumberOfRounds { get; set; }
+
+    [FromForm(Name = "release_stage")]
+    public required string ReleaseStage { get; set; }
+
+    [FromForm(Name = "banned_heroes")]
+    public required string BannedHeroes { get; set; }
+
+    [FromForm(Name = "awd_mann")]
+    public required int AwardMostAnnihilations { get; set; }
+
+    [FromForm(Name = "awd_mqk")]
+    public required int AwardMostQuadKills { get; set; }
+
+    [FromForm(Name = "awd_lgks")]
+    public required int AwardLargestKillStreak { get; set; }
+
+    [FromForm(Name = "awd_msd")]
+    public required int AwardMostSmackdowns { get; set; }
+
+    [FromForm(Name = "awd_mkill")]
+    public required int AwardMostKills { get; set; }
+
+    [FromForm(Name = "awd_masst")]
+    public required int AwardMostAssists { get; set; }
+
+    [FromForm(Name = "awd_ledth")]
+    public required int AwardLeastDeaths { get; set; }
+
+    [FromForm(Name = "awd_mbdmg")]
+    public required int AwardMostBuildingDamage { get; set; }
+
+    [FromForm(Name = "awd_mwk")]
+    public required int AwardMostWardsKilled { get; set; }
+
+    [FromForm(Name = "awd_mhdd")]
+    public required int AwardMostHeroDamageDealt { get; set; }
+
+    [FromForm(Name = "awd_hcs")]
+    public required int AwardHighestCreepScore { get; set; }
+
+    [FromForm(Name = "submission_debug")]
+    public required string SubmissionDebug { get; set; }
 }
 
-// [Index(nameof(match_id), IsUnique = false)]
-public class PlayerMatchResults
+public class IndividualPlayerStats
 {
-    public PlayerMatchResults()
-    {
-    }
-
-    public PlayerMatchResults(List<PlayerMatchResults> other)
-    {
-        wins = other.Sum(r => r.wins);
-        losses = other.Sum(r => r.losses);
-        discos = other.Sum(r => r.discos);
-        concedes = other.Sum(r => r.concedes);
-        kicked = other.Sum(r => r.kicked);
-        social_bonus = other.Sum(r => r.social_bonus);
-        used_token = other.Sum(r => r.used_token);
-        pub_skill = other.Sum(r => r.pub_skill);
-        pub_count = other.Sum(r => r.pub_count);
-        amm_team_rating = other.Sum(r => r.amm_team_rating);
-        amm_team_count = other.Sum(r => r.amm_team_count);
-        concedevotes = other.Sum(r => r.concedevotes);
-        herokills = other.Sum(r => r.herokills);
-        herodmg = other.Sum(r => r.herodmg);
-        herokillsgold = other.Sum(r => r.herokillsgold);
-        heroassists = other.Sum(r => r.heroassists);
-        heroexp = other.Sum(r => r.heroexp);
-        deaths = other.Sum(r => r.deaths);
-        buybacks = other.Sum(r => r.buybacks);
-        goldlost2death = other.Sum(r => r.goldlost2death);
-        secs_dead = other.Sum(r => r.secs_dead);
-        teamcreepkills = other.Sum(r => r.teamcreepkills);
-        teamcreepdmg = other.Sum(r => r.teamcreepdmg);
-        teamcreepgold = other.Sum(r => r.teamcreepgold);
-        teamcreepexp = other.Sum(r => r.teamcreepexp);
-        neutralcreepkills = other.Sum(r => r.neutralcreepkills);
-        neutralcreepdmg = other.Sum(r => r.neutralcreepdmg);
-        neutralcreepgold = other.Sum(r => r.neutralcreepgold);
-        neutralcreepexp = other.Sum(r => r.neutralcreepexp);
-        bdmg = other.Sum(r => r.bdmg);
-        razed = other.Sum(r => r.razed);
-        bdmgexp = other.Sum(r => r.bdmgexp);
-        bgold = other.Sum(r => r.bgold);
-        denies = other.Sum(r => r.denies);
-        exp_denied = other.Sum(r => r.exp_denied);
-        gold = other.Sum(r => r.gold);
-        gold_spent = other.Sum(r => r.gold_spent);
-        exp = other.Sum(r => r.exp);
-        actions = other.Sum(r => r.actions);
-        secs = other.Sum(r => r.secs);
-        level = other.Sum(r => r.level);
-        consumables = other.Sum(r => r.consumables);
-        wards = other.Sum(r => r.wards);
-        bloodlust = other.Sum(r => r.bloodlust);
-        doublekill = other.Sum(r => r.doublekill);
-        triplekill = other.Sum(r => r.triplekill);
-        quadkill = other.Sum(r => r.quadkill);
-        annihilation = other.Sum(r => r.annihilation);
-        ks3 = other.Sum(r => r.ks3);
-        ks4 = other.Sum(r => r.ks4);
-        ks5 = other.Sum(r => r.ks5);
-        ks6 = other.Sum(r => r.ks6);
-        ks7 = other.Sum(r => r.ks7);
-        ks8 = other.Sum(r => r.ks8);
-        ks9 = other.Sum(r => r.ks9);
-        ks10 = other.Sum(r => r.ks10);
-        ks15 = other.Sum(r => r.ks15);
-        smackdown = other.Sum(r => r.smackdown);
-        humiliation = other.Sum(r => r.humiliation);
-        nemesis = other.Sum(r => r.nemesis);
-        retribution = other.Sum(r => r.retribution);
-        score = other.Sum(r => r.score);
-        gameplaystat0 = other.Sum(r => r.gameplaystat0);
-        gameplaystat1 = other.Sum(r => r.gameplaystat1);
-        gameplaystat2 = other.Sum(r => r.gameplaystat2);
-        gameplaystat3 = other.Sum(r => r.gameplaystat3);
-        gameplaystat4 = other.Sum(r => r.gameplaystat4);
-        gameplaystat5 = other.Sum(r => r.gameplaystat5);
-        gameplaystat6 = other.Sum(r => r.gameplaystat6);
-        gameplaystat7 = other.Sum(r => r.gameplaystat7);
-        gameplaystat8 = other.Sum(r => r.gameplaystat8);
-        gameplaystat9 = other.Sum(r => r.gameplaystat9);
-        time_earning_exp = other.Sum(r => r.time_earning_exp);
-
-        if (other.Count == 0)
-        {
-            // Should not happen.
-            map = null;
-        }
-
-        else
-        {
-            map = other[0].map;
-        }
-    }
-
     public string? nickname { get; set; }
     public string? clan_tag { get; set; }
     public int clan_id { get; set; }
