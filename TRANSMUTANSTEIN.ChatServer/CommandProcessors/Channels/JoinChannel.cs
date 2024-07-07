35cd3070d70fd9a043da3bf3faf2cf11ef45d93f
[K]ONGOR <project.kongor@proton.me>
2024-07-08 00:06:35 +0100

    Intercept Channel Join Requests

---
+++ TRANSMUTANSTEIN.ChatServer/CommandProcessors/Channels/JoinChannel.cs
@@ -0,0 +1,18 @@
+﻿namespace TRANSMUTANSTEIN.ChatServer.CommandProcessors.Channels;
+
+[ChatCommand(ChatProtocol.Command.CHAT_CMD_JOIN_CHANNEL)]
+public class JoinChannel(MerrickContext merrick, ILogger<JoinChannel> logger) : CommandProcessorsBase, ICommandProcessor
+{
+    private MerrickContext MerrickContext { get; set; } = merrick;
+    private ILogger<JoinChannel> Logger { get; set; } = logger;
+
+    public async Task Process(TCPSession session, ChatBuffer buffer)
+    {
+        byte[] _ = buffer.ReadCommandBytes();
+        string channel = buffer.ReadString();
+
+        // TODO: Handle Max Channels
+
+        // TODO: Handle Channel Passwords And Permissions
+    }
+}
