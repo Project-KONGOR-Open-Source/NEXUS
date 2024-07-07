322a80355a3295407ba5efa1ef2765fcaaa0b7da
[K]ONGOR <project.kongor@proton.me>
2024-07-07 22:02:04 +0100

    [WIP] Attempt To Fix Chat Server Frames

--- TRANSMUTANSTEIN.ChatServer/Core/ChatSession.cs
+++ TRANSMUTANSTEIN.ChatServer/Core/ChatSession.cs
@@ -17,16 +17,60 @@ public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : T
         Logger.LogInformation($"Chat Session ID {ID} Has Terminated");
     }
 
+    private ushort? _expectedDataSize = null;
+
     protected override void OnReceived(byte[] buffer, long offset, long size)
     {
-        ushort command = BitConverter.ToUInt16([buffer[0], buffer[1]]);
+        var debug1 = buffer;
+        var debug2 = this.BytesPending;
+        var debug3 = this.BytesReceived;
+        var debug4 = this.BytesSent;
+
+
+        byte[] frame = buffer[..(int)BytesReceived];
+
+        if (size < 2)
+            throw new Exception("Invalid Frame Size (command etc.)");
+
+        else if (size == 2 && _expectedDataSize == null)
+        {
+            _expectedDataSize = BitConverter.ToUInt16([frame[0], frame[1]]);
+        }
+
+        else if (size == _expectedDataSize)
+        {
+            _expectedDataSize = null;
+            ProcessFrame(frame);
+        }
+
+        else if (_expectedDataSize == null || _expectedDataSize < size)
+        {
+            List<byte[]> frames = _expectedDataSize == null ? GetFrames(frame) : GetFrames(BitConverter.GetBytes(_expectedDataSize.Value).Concat(frame).ToArray());
+
+            Parallel.ForEach(frames, ProcessFrame);
+        }
+
+        else
+        {
+            throw new Exception("[BUG]");
+        }
+    }
+
+    protected override void OnError(SocketError error)
+    {
+        Logger.LogInformation($"Chat Session ID {ID} Caught A Socket Error With Code {error}");
+    }
+
+    private void ProcessFrame(byte[] frame)
+    {
+        ushort command = BitConverter.ToUInt16([frame[0], frame[1]]);
 
         Type? commandType = GetCommandType(command);
 
         if (commandType is null)
         {
             string output = new StringBuilder($@"Missing Command Type For Command: ""{command:X4}""")
-                .Append(Environment.NewLine).Append($"Message UTF8 Text: {Encoding.UTF8.GetString(buffer, (int)offset, (int)size - (int)offset)}").ToString();
+                .Append(Environment.NewLine).Append($"Message UTF8 Text: {Encoding.UTF8.GetString(frame)}").ToString();
 
             Logger.LogError(output);
         }
@@ -34,7 +78,7 @@ public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : T
         else
         {
             if (GetCommandTypeInstance(commandType) is { } commandTypeInstance)
-                commandTypeInstance.Process(this, new ChatBuffer(buffer));
+                commandTypeInstance.Process(this, new ChatBuffer(frame));
 
             else Logger.LogError($@"[BUG] Could Not Create Command Type Instance For Command: ""{command:X4}""");
         }
@@ -48,7 +92,7 @@ public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : T
 
             type = types
                 .SingleOrDefault(type => type.GetCustomAttribute<ChatCommandAttribute>() is not null
-                    && (type.GetCustomAttribute<ChatCommandAttribute>()?.Command.Equals(command) ?? false));
+                                         && (type.GetCustomAttribute<ChatCommandAttribute>()?.Command.Equals(command) ?? false));
 
             if (type is not null)
                 CommandToTypeMap.Add(command, type);
@@ -64,8 +108,22 @@ public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : T
         }
     }
 
-    protected override void OnError(SocketError error)
+    private List<byte[]> GetFrames(byte[] buffer)
     {
-        Logger.LogInformation($"Chat Session ID {ID} Caught A Socket Error With Code {error}");
+        List<byte[]> frames = [];
+
+        int offset = 0;
+
+        while (offset < buffer.Length)
+        {
+            ushort frameSize = BitConverter.ToUInt16([buffer[offset], buffer[offset + 1]]);
+            byte[] frame = buffer[(offset + 2)..(offset + 2 + frameSize)];
+
+            frames.Add(frame);
+
+            offset += 2 + frameSize;
+        }
+
+        return frames;
     }
 }
