f2b3611fe64f1f9f67a48f598f7e3baffd73517c
[K]ONGOR <project.kongor@proton.me>
2024-07-11 22:42:48 +0100

    Fix TCP Data Processor

--- TRANSMUTANSTEIN.ChatServer/Core/ChatSession.cs
+++ TRANSMUTANSTEIN.ChatServer/Core/ChatSession.cs
@@ -3,11 +3,12 @@
 public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : TCPSession(server)
 {
     private IServiceProvider ServiceProvider { get; set; } = serviceProvider;
+
     private ILogger Logger { get; } = serviceProvider.GetRequiredService<ILogger<ChatSession>>();
 
     private static ConcurrentDictionary<ushort, Type> CommandToTypeMap { get; set; } = [];
 
-    private byte[]? ExpectedDataSize { get; set; }
+    private byte[] RemainingPreviouslyReceivedData { get; set; } = [];
 
     protected override void OnConnected()
     {
@@ -26,43 +27,45 @@ public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : T
 
     protected override void OnReceived(byte[] buffer, long offset, long size)
     {
-        byte[] received = buffer[(int)offset..(int)size];
+        byte[] received = RemainingPreviouslyReceivedData.Concat(buffer[(int)offset..(int)size]).ToArray();
 
-        // If The Data Size Is Less Than 2 Bytes, Then It Is Not A Valid Data Segment
-        if (size < 2) Logger.LogError("Received Data Size Is Less Than 2 Bytes");
+        List<byte[]> segments = ExtractDataSegments(received, out byte[] remaining);
 
-        // If The Data Size Is 2 Bytes, Then It Is The Expected Data Size For The Next Data Segment To Be Sent
-        else if (size == 2 && ExpectedDataSize == null)
+        RemainingPreviouslyReceivedData = remaining;
+
+        Parallel.ForEach(segments, ProcessDataSegment);
+    }
+
+    private List<byte[]> ExtractDataSegments(byte[] buffer, out byte[] remaining)
+    {
+        remaining = [];
+
+        List<byte[]> segments = [];
+
+        int offset = 0;
+
+        while (offset < buffer.Length)
         {
-            if (received.Length != 2)
-            {
-                Logger.LogError($"[BUG] Received Data Size Is Expected To Be 2 Bytes, But It Is {received.Length}");
+            ushort size = BitConverter.ToUInt16([buffer[offset + 0], buffer[offset + 1]]);
 
-                return;
+            if (size + 2 <= buffer.Length - offset)
+            {
+                byte[] segment = buffer[(offset + 2)..(offset + 2 + size)];
+
+                segments.Add(segment);
+
+                offset += 2 + size;
             }
 
-            ExpectedDataSize = received;
+            else
+            {
+                remaining = buffer[offset..buffer.Length];
+
+                offset = buffer.Length;
+            }
         }
 
-        // If The Received Data Size Matches The Expected Data Size, Then We Can Process The Data Segment
-        else if (ExpectedDataSize != null && size == Convert.ToInt64(BitConverter.ToUInt16([ExpectedDataSize.First(), ExpectedDataSize.Last()])))
-        {
-            ExpectedDataSize = null;
-            ProcessDataSegment(received);
-        }
-
-        // The Received Data May Contain Multiple Data Segments, So We Need To Split Them And Process Each One Individually
-        else if (ExpectedDataSize == null || size > Convert.ToInt64(BitConverter.ToUInt16([ExpectedDataSize.First(), ExpectedDataSize.Last()])))
-        {
-            List<byte[]> segments = ExpectedDataSize == null ? GetDataSegments(received) : GetDataSegments(ExpectedDataSize.Concat(received).ToArray());
-
-            ExpectedDataSize = null;
-
-            Parallel.ForEach(segments, ProcessDataSegment);
-        }
-
-        // If The Received Data Does Not Match Any Of The Previous Conditions, Then It Is Not Valid Data
-        else Logger.LogError($"[BUG] Unable To Process Bytes {string.Join(':', received)}");
+        return segments;
     }
 
     private void ProcessDataSegment(byte[] segment)
@@ -73,8 +76,10 @@ public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : T
 
         if (commandType is null)
         {
-            string output = new StringBuilder($@"Missing Command Type For Command: ""{command:X4}""")
-                .Append(Environment.NewLine).Append($"Message UTF8 Text: {Encoding.UTF8.GetString(segment)}").ToString();
+            string output = new StringBuilder($@"Missing Type Mapping For Command: ""{command:X4}""")
+                .Append(Environment.NewLine).Append($"Message UTF8 Bytes: {string.Join(':', segment)}")
+                .Append(Environment.NewLine).Append($"Message UTF8 Text: {Encoding.UTF8.GetString(segment)}")
+                .ToString();
 
             Logger.LogError(output);
         }
@@ -110,33 +115,5 @@ public class ChatSession(TCPServer server, IServiceProvider serviceProvider) : T
         object instance = ActivatorUtilities.CreateInstance(ServiceProvider, type);
 
         return instance as ICommandProcessor;
-    }
-
-    private List<byte[]> GetDataSegments(byte[] buffer)
-    {
-        List<byte[]> segments = [];
-
-        int offset = 0;
-
-        while (offset < buffer.Length)
-        {
-            try
-            {
-                ushort segmentSize = BitConverter.ToUInt16([buffer[offset + 0], buffer[offset + 1]]);
-                byte[] segment = buffer[(offset + 2)..(offset + 2 + segmentSize)];
-
-                segments.Add(segment);
-
-                offset += 2 + segmentSize;
-            }
-
-            catch (Exception exception)
-            {
-                Logger.LogError(exception, $"Failed To Get Segment From Buffer With Remaining Bytes {string.Join(':', buffer[offset..buffer.Length])}");
-                break;
-            }
-        }
-
-        return segments;
     }
 }
