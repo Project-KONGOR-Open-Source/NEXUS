f8833a3f35f6f56b269a750645d89bc67fc9f80e
[K]ONGOR <project.kongor@proton.me>
2024-07-20 20:39:08 +0100

    Intercept User Action Events

---
+++ TRANSMUTANSTEIN.ChatServer/CommandProcessors/Actions/TrackPlayerAction.cs
@@ -0,0 +1,23 @@
+﻿namespace TRANSMUTANSTEIN.ChatServer.CommandProcessors.Actions;
+
+[ChatCommand(ChatProtocol.Command.CHAT_CMD_TRACK_PLAYER_ACTION)]
+public class TrackPlayerAction(MerrickContext merrick, ILogger<TrackPlayerAction> logger) : CommandProcessorsBase, ICommandProcessor
+{
+    private MerrickContext MerrickContext { get; set; } = merrick;
+    private ILogger<TrackPlayerAction> Logger { get; set; } = logger;
+
+    public async Task Process(ChatSession session, ChatBuffer buffer)
+    {
+        TrackPlayerActionRequestData requestData = new(buffer);
+
+        // TODO: Do Something With This Data
+
+        Logger.LogError($@"Unhandled User Action: ""{requestData.Action}""");
+    }
+}
+
+public class TrackPlayerActionRequestData(ChatBuffer buffer)
+{
+    public byte[] CommandBytes = buffer.ReadCommandBytes();
+    public ChatProtocol.ActionCampaign Action = (ChatProtocol.ActionCampaign)buffer.ReadInt8();
+}
