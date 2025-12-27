92c318fa8d8cbdb85e5343cca196fe1b260c7022
[K]ONGOR <project.kongor@proton.me>
2025-12-27 17:09:37 +0000

    Add Match Status And Match Complete Placeholders

---
+++ TRANSMUTANSTEIN.ChatServer/CommandProcessors/MatchState/MatchComplete.cs
@@ -0,0 +1,40 @@
+namespace TRANSMUTANSTEIN.ChatServer.CommandProcessors.MatchState;
+
+[ChatCommand(ChatProtocol.GameServerToChatServer.NET_CHAT_GS_MATCH_ENDED)]
+public class MatchComplete : ISynchronousCommandProcessor<MatchServerChatSession>
+{
+    public void Process(MatchServerChatSession session, ChatBuffer buffer)
+    {
+        MatchCompleteRequestData requestData = new (buffer);
+
+        // TODO: Update Player Availability States (Mark Players As Available After Match Ends)
+        // TODO: Remove Match From Distributed Cache
+        // TODO: Mark Server As Available For New Match Allocation
+        // TODO: Notify Players That Match Has Ended
+        // TODO: Clean Up Match-Related Session State
+
+        // NOTE: Statistics submission and MMR/PSR updates are handled by KONGOR.MasterServer/Controllers/StatsRequesterController.
+    }
+}
+
+file class MatchCompleteRequestData
+{
+    public byte[] CommandBytes { get; init; }
+
+    public int ServerID { get; init; }
+
+    public int MatchID { get; init; }
+
+    public int WinningTeam { get; init; }
+
+    public int MatchDuration { get; init; }
+
+    public MatchCompleteRequestData(ChatBuffer buffer)
+    {
+        CommandBytes = buffer.ReadCommandBytes();
+        ServerID = buffer.ReadInt32();
+        MatchID = buffer.ReadInt32();
+        WinningTeam = buffer.ReadInt32();
+        MatchDuration = buffer.ReadInt32();
+    }
+}
---
+++ TRANSMUTANSTEIN.ChatServer/CommandProcessors/MatchState/MatchStatus.cs
@@ -0,0 +1,36 @@
+namespace TRANSMUTANSTEIN.ChatServer.CommandProcessors.MatchState;
+
+[ChatCommand(ChatProtocol.GameServerToChatServer.NET_CHAT_GS_MATCH_ONGOING)]
+public class MatchStatus : ISynchronousCommandProcessor<MatchServerChatSession>
+{
+    public void Process(MatchServerChatSession session, ChatBuffer buffer)
+    {
+        MatchStatusRequestData requestData = new (buffer);
+
+        // TODO: Update Match Phase In Distributed Cache (In Progress, Paused, Resumed)
+        // TODO: Update Player Availability States (Mark Players As InGame When Match Starts)
+        // TODO: Track Match Duration For Statistics And Timeout Detection
+    }
+}
+
+file class MatchStatusRequestData
+{
+    public byte[] CommandBytes { get; init; }
+
+    public int ServerID { get; init; }
+
+    public int MatchID { get; init; }
+
+    public int Phase { get; init; }
+
+    public int CurrentGameTime { get; init; }
+
+    public MatchStatusRequestData(ChatBuffer buffer)
+    {
+        CommandBytes = buffer.ReadCommandBytes();
+        ServerID = buffer.ReadInt32();
+        MatchID = buffer.ReadInt32();
+        Phase = buffer.ReadInt32();
+        CurrentGameTime = buffer.ReadInt32();
+    }
+}
