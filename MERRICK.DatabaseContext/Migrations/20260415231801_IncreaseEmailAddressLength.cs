b74529361c017646a3a8ccedec7a604602e40b33
[K]ONGOR <project.kongor@proton.me>
2026-04-16 00:20:39 +0100

    Add Migration For Email Address Length Increase

---
+++ MERRICK.DatabaseContext/Migrations/20260415231801_IncreaseEmailAddressLength.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MERRICK.DatabaseContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncreaseEmailAddressLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailAddress",
+                schema: "core",
+                table: "Users",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmailAddress",
+                schema: "core",
+                table: "Users",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(128)",
+                oldMaxLength: 128);
+        }
+    }
+}
