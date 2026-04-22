9bd4c0e89bc0d85cd937662f75c5f659642a1f11
[K]ONGOR <project.kongor@proton.me>
2026-04-22 22:04:04 +0100

    Remove Source Generator

--- ASPIRE.SourceGenerator/Internals/UsingDirectives.cs
+++
@@ -1,8 +0,0 @@
-﻿global using ASPIRE.SourceGenerator.Attributes;
-
-global using Microsoft.CodeAnalysis.CSharp.Syntax;
-global using Microsoft.CodeAnalysis.CSharp;
-global using Microsoft.CodeAnalysis;
-
-global using System.Collections.Immutable;
-global using System.Text;
