e2bdd0909e08eb1b83d986bead55ad5d096fd94f
[K]ONGOR <project.kongor@proton.me>
2026-05-29 10:01:08 +0100

    Update Service Defaults

--- ASPIRE.Common/ServiceDefaults/ServiceExtensions.cs
+++ ASPIRE.Common/ServiceDefaults/ServiceExtensions.cs
@@ -2,7 +2,7 @@ namespace ASPIRE.Common.ServiceDefaults;
 
 // Adds common Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
-// To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
+// To learn more about using this project, see https://aka.ms/aspire/service-defaults
 public static class ServiceExtensions
 {
     private const string HealthEndpointPath = "/health";
