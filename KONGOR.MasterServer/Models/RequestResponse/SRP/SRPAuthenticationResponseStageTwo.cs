59bc6497f2022cc00c5dedf1e506ec0765d91430
[K]ONGOR <project.kongor@proton.me>
2026-06-03 13:35:42 +0100

    Update Incorrect Notification Format Description

--- KONGOR.MasterServer/Models/RequestResponse/SRP/SRPAuthenticationResponseStageTwo.cs
+++ KONGOR.MasterServer/Models/RequestResponse/SRP/SRPAuthenticationResponseStageTwo.cs
@@ -1405,15 +1405,15 @@ public class Notification
     /// <summary>
     ///     A pipe-separated set of notification data.
     ///     <br/>
-    ///     The format is: "{SenderAccountName}|{Unknown}|{NotificationStatus}|{NotificationType}|{NotificationDisplayType}|{NotificationAction}|{NotificationTimestamp}|{NotificationID}".
+    ///     The format is: "{SenderAccountName}|{Unknown}|{NotificationType}|{NotificationTranslationString}|{NotificationDisplayType}|{NotificationAction}|{NotificationTimestamp}|{NotificationID}".
     ///     <br/>
-    ///     The notification status can be either 0 = Removable, 1 = Not Seen, 2 = Seen. The other data points are exemplified below.
+    ///     The notification type is a numeric value, for example 2 = buddy added, 22 = outgoing friend request, or 23 = incoming friend request. The other data points are exemplified below.
     ///     <code>
     ///         Examples (the spaces are only added for readability, but they are not needed):
     ///             "KONGOR||23|notify_buddy_requested_added|notification_generic_action|action_friend_request|01/18 00:21 AM|5000001"
     ///             "KONGOR|| 2|notify_buddy_added          |notification_generic_info  |                     |01/18 00:22 AM|5000002"
-    ///             "KONGOR|| 2|notify_buddy_requested_adder|notification_generic_info  |                     |01/18 00:23 AM|5000003"
-    ///             "KONGOR|| 2|notify_replay_available     |notification_generic_info  |                     |01/18 00:24 AM|5000004"
+    ///             "KONGOR||22|notify_buddy_requested_adder|notification_generic_info  |                     |01/18 00:23 AM|5000003"
+    ///             "KONGOR||26|notify_replay_available     |notification_generic_info  |                     |01/18 00:24 AM|5000004"
     ///     </code>
     /// </summary>
     [PHPProperty("notification")]
