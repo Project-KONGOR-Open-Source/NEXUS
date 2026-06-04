8e4534bae668a712f3f1155c194e3a87810e5d57
[K]ONGOR <project.kongor@proton.me>
2026-06-04 20:16:03 +0100

    Rename Database

--- ASPIRE.ApplicationHost/ASPIRE.cs
+++ ASPIRE.ApplicationHost/ASPIRE.cs
@@ -79,7 +79,7 @@ public class ASPIRE
         // Add SQL Server Resource (SQL Server 2025 Requires A Container-Native Data Directory, A Linux-To-Windows Bind Mount Causes I/O Failures During Startup, So A Data Volume Is Required)
         IResourceBuilder<SqlServerServerResource> databaseServer = builder.AddSqlServer("database-server", password: databasePassword, port: databasePort)
             .WithImageTag("latest") // SQL Server Image Tags: https://mcr.microsoft.com/en-gb/artifact/mar/mssql/server/tags
-            .WithLifetime(ContainerLifetime.Persistent).WithDataVolume($"database-server-{databaseName}") // Persist SQL Server Data As Docker-Managed Data Volume
+            .WithLifetime(ContainerLifetime.Persistent).WithDataVolume($"database-server-data-{databaseName}") // Persist SQL Server Data As Docker-Managed Data Volume
             .WithEnvironment("ACCEPT_EULA", "Y").WithEnvironment("MSSQL_PID", "Developer"); // SQL Server Image Information: https://mcr.microsoft.com/en-gb/artifact/mar/mssql/server/about
 
         // Create Resource Relationship After Parent Resource Is Defined
