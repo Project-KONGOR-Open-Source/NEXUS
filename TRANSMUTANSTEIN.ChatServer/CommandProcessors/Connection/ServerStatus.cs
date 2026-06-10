666c0f06464643dfea2e5013e66e6d9f38ca148c
[K]ONGOR <project.kongor@proton.me>
2026-06-10 17:17:55 +0100

    Add Remote Endpoint To NULL Match Server Log Entry

--- TRANSMUTANSTEIN.ChatServer/CommandProcessors/Connection/ServerStatus.cs
+++ TRANSMUTANSTEIN.ChatServer/CommandProcessors/Connection/ServerStatus.cs
@@ -14,8 +14,10 @@ public class ServerStatus(IDatabase distributedCacheStore) : IAsynchronousComman
 
         if (matchServer is null)
         {
-            Log.Error(@"[BUG] Received Status Update For Unknown Match Server ID ""{MatchServerID}""" + Environment.NewLine + @"Name: ""{MatchServerName}"", Address: ""{MatchServerAddress}:{MatchServerPort}"", HostName: ""{MatchServerHostName}"", MatchID: ""{MatchID}"", SlaveID: ""{SlaveID}"", Status: {Status}",
-                requestData.ServerID, requestData.Name, requestData.Address, requestData.Port, requestData.HostName, requestData.MatchID, requestData.SlaveID, requestData.Status);
+            string? remoteEndPoint = session.Socket.RemoteEndPoint?.ToString();
+
+            Log.Error(@"[BUG] Received Status Update For Unknown Match Server ID ""{MatchServerID}""" + Environment.NewLine + @"RemoteEndPoint: ""{RemoteEndPoint}"", Name: ""{MatchServerName}"", Address: ""{MatchServerAddress}:{MatchServerPort}"", HostName: ""{MatchServerHostName}"", MatchID: ""{MatchID}"", SlaveID: ""{SlaveID}"", Status: {Status}",
+                requestData.ServerID, remoteEndPoint, requestData.Name, requestData.Address, requestData.Port, requestData.HostName, requestData.MatchID, requestData.SlaveID, requestData.Status);
 
             return;
         }
