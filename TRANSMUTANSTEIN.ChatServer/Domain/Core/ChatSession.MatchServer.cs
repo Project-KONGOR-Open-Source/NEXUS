d31e8058a0205612cc4d8b471389e141c31dfbfe
[K]ONGOR <project.kongor@proton.me>
2026-06-13 20:02:25 +0100

    Consider Sleeping Match Servers To Be Available

--- TRANSMUTANSTEIN.ChatServer/Domain/Core/ChatSession.MatchServer.cs
+++ TRANSMUTANSTEIN.ChatServer/Domain/Core/ChatSession.MatchServer.cs
@@ -1,4 +1,4 @@
-﻿namespace TRANSMUTANSTEIN.ChatServer.Domain.Core;
+namespace TRANSMUTANSTEIN.ChatServer.Domain.Core;
 
 public class MatchServerChatSession(TCPServer server, IServiceProvider serviceProvider) : ChatSession(server, serviceProvider)
 {
@@ -223,7 +223,7 @@ public class MatchServerChatSession(TCPServer server, IServiceProvider servicePr
     /// </summary>
     public bool IsAvailable()
     {
-        return Metadata.Status is ChatProtocol.ServerStatus.SERVER_STATUS_IDLE && WasRecentlyUsed() is false;
+        return (Metadata.Status is ChatProtocol.ServerStatus.SERVER_STATUS_SLEEPING or ChatProtocol.ServerStatus.SERVER_STATUS_IDLE) && WasRecentlyUsed() is false;
     }
 
     /// <summary>
