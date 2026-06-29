8152e28aa60103f6c96dd8feae23a26f22a65aec
[K]ONGOR <project.kongor@proton.me>
2026-06-29 22:45:37 +0100

    Remove Clan Message Data Transfer Object

--- ZORGATH.WebPortal.API/Contracts/ClanDTOs.cs
+++ ZORGATH.WebPortal.API/Contracts/ClanDTOs.cs
@@ -6,11 +6,6 @@ namespace ZORGATH.WebPortal.API.Contracts;
 public record SetClanDetailsDTO(string Title, string Logo);
 
 /// <summary>
-///     The request payload for updating the authenticated account's per-member clan message.
-/// </summary>
-public record SetClanMessageDTO(string Message);
-
-/// <summary>
 ///     The clan details returned after a successful update.
 /// </summary>
 public record ClanDetailsDTO(string Name, string Tag, string Title, string Logo);
