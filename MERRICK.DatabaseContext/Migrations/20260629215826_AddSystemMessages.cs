da06dcd54219398964023a505ecb004313b7e95e
[K]ONGOR <project.kongor@proton.me>
2026-06-29 23:00:30 +0100

    Add System Messages Migration

---
+++ MERRICK.DatabaseContext/Migrations/20260629215826_AddSystemMessages.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MERRICK.DatabaseContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSystemMessages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                schema: "core",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AccountID = table.Column<int>(type: "int", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Subtitle = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    BodyTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    Footer = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Read = table.Column<bool>(type: "bit", nullable: false),
+                    Deletable = table.Column<bool>(type: "bit", nullable: false),
+                    TimestampSent = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    TimestampExpires = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Messages_Accounts_AccountID",
+                        column: x => x.AccountID,
+                        principalSchema: "core",
+                        principalTable: "Accounts",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Messages_AccountID",
+                schema: "core",
+                table: "Messages",
+                column: "AccountID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Messages",
+                schema: "core");
+        }
+    }
+}
