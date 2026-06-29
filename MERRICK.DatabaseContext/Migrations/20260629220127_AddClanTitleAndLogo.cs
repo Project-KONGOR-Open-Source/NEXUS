98e6d1998545be6b7f5f45bb1194f8c7127660ab
[K]ONGOR <project.kongor@proton.me>
2026-06-29 23:02:06 +0100

    Add Clan Title And Logo Migration

---
+++ MERRICK.DatabaseContext/Migrations/20260629220127_AddClanTitleAndLogo.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MERRICK.DatabaseContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClanTitleAndLogo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Logo",
+                schema: "core",
+                table: "Clans",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                schema: "core",
+                table: "Clans",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Logo",
+                schema: "core",
+                table: "Clans");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                schema: "core",
+                table: "Clans");
+        }
+    }
+}
